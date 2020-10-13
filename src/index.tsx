0bf07c7c89e9153857f513e0113721885d8f7a1e
Victor Alcântara <victoralcan@gmail.com>
2020-10-13 15:59:00 -0300

    :fire: :arrow_up: Adicionando dependencias. Removendo arquivos não utilizados

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import initStore from './config/store';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,8 +15,3 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
