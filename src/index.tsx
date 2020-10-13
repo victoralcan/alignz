c4c14eba7161ccd2afd8ea60a6f54abbebab7adf
Victor Alcântara <victoralcan@gmail.com>
2020-10-13 13:46:43 -0300

    :sparkles: Adicionando bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import initStore from './config/store';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = initStore();
 
