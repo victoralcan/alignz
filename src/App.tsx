96df0e7ce16264542593a91b3e08e2b1831c1b7f
Victor Alcântara <victoralcan@gmail.com>
2020-10-13 19:57:43 -0300

    :construction: :art: [#1] Adicionando logo ao header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Header from './shared/components/header';
+import Header from './shared/components/header/header';
 import Routes from './routes';
 import Footer from './shared/components/footer';
 
--- src/config/store.ts
+++ src/config/store.ts
@@ -4,10 +4,7 @@ import thunkMiddleware from 'redux-thunk';
 import reducer, { IRootState } from '../shared/reducers';
 
 const defaultMiddlewares = [thunkMiddleware, promiseMiddleware];
-const composedMiddlewares = (middlewares) =>
-    process.env.NODE_ENV === 'development'
-        ? compose(applyMiddleware(...defaultMiddlewares, ...middlewares))
-        : compose(applyMiddleware(...defaultMiddlewares, ...middlewares));
+const composedMiddlewares = (middlewares) => compose(applyMiddleware(...defaultMiddlewares, ...middlewares));
 
 const initialize = (initialState?: IRootState, middlewares = []) =>
     createStore(reducer, initialState, composedMiddlewares(middlewares));
