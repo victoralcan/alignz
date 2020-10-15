feac6ad1d91974eaf018fb4d5891579111dc69bc
Victor Alcântara <victoralcan@gmail.com>
2020-10-15 19:14:47 -0300

    :sparkles: [#1] Adicionando as páginas

---
+++ src/pages/aparelhoInvisivel.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AparelhoInvisivel() {
+    return (
+        <div>
+            <div>Aparelho Invisivel</div>
+        </div>
+    );
+}
+
+export default AparelhoInvisivel;
---
+++ src/pages/clareamento.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Clareamento() {
+    return (
+        <div>
+            <div>Clareamento</div>
+        </div>
+    );
+}
+
+export default Clareamento;
