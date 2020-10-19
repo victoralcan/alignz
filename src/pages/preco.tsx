b1d40ce765488e0fa60b55963ad575a91bdb7023
Victor Alcântara <victoralcan@gmail.com>
2020-10-19 20:53:32 -0300

    :bug: Ajustando row e col

--- src/pages/preco.tsx
+++ src/pages/preco.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Row } from 'reactstrap';
 
 function Preco() {
     return (
-        <div>
+        <Row>
             <div>Preco</div>
-        </div>
+        </Row>
     );
 }
 
