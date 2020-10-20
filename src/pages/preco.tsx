68b7fd89e03c2e34161914ea00ec74e47941c9da
Victor Alcântara <victoralcan@gmail.com>
2020-10-19 23:18:36 -0300

    :construction: [#6] Aprimorando tela de preço

--- src/pages/preco.tsx
+++ src/pages/preco.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import { Col, Row } from 'reactstrap';
+import { Button, Card, CardBody, CardHeader, Col, Input, Row } from 'reactstrap';
 import FittedImage from 'react-fitted-image';
 import Preco1 from 'content/images/preco1.jpg';
+import '../styles/preco.css';
 
 function Preco() {
+  const backgroundColor = '#fee0cc';
   return (
     <>
       <Row className="infoContainer">
         <Col md={6} className="h-100">
           <FittedImage fit="cover" src={Preco1} />
         </Col>
-        <Col md={6}>Email</Col>
+        <Col className="d-flex justify-content-center align-items-center" md={6} style={{ backgroundColor }}>
+          <Card style={{ backgroundColor, margin: '0px 100px 0px 100px' }}>
+            <CardHeader className="text-center border-0" style={{ backgroundColor, color: '#77C1CD' }}>
+              <h1 className="font-weight-bold">Receba o preço do nosso aparelho invisível ;)</h1>
+            </CardHeader>
+            <CardBody style={{ backgroundColor }}>
+              <Input placeholder="Insira seu email" className="mb-3" />
+              <div className="d-flex justify-content-center">
+                <Button style={{ backgroundColor: '#F48472' }} className="border-0 btn-lg">
+                  Solicitar o Preço
+                </Button>
+              </div>
+            </CardBody>
+          </Card>
+        </Col>
       </Row>
     </>
   );
