f12f19874cf52001ecde533d495da2686d98c370
Victor Alcântara <victoralcan@gmail.com>
2020-10-21 17:32:23 -0300

    :arrow_up: [#6] Adicionando ícones

---
+++ src/config/icon-loader.ts
@@ -0,0 +1,7 @@
+import { faDollarSign } from '@fortawesome/free-solid-svg-icons/faDollarSign';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+export const loadIcons = () => {
+  library.add(faDollarSign);
+};
