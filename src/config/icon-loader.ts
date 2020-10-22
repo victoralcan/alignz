14cdc0217c76d1ee2ebac813c8b9b703ad824046
Victor Alcântara <victoralcan@gmail.com>
2020-10-21 23:50:52 -0300

    :construction: :lipstick: [#6] Adicionando ícone aos depoimentos

--- src/config/icon-loader.ts
+++ src/config/icon-loader.ts
@@ -1,7 +1,9 @@
 import { faDollarSign } from '@fortawesome/free-solid-svg-icons/faDollarSign';
+import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
+import { faMinus } from '@fortawesome/free-solid-svg-icons/faMinus';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 export const loadIcons = () => {
-  library.add(faDollarSign);
+  library.add(faDollarSign, faPlus, faMinus);
 };
