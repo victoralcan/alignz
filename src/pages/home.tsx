625f4b92c1aaffc58ab7e7919473ba2518f8416c
Victor Alcântara <victoralcan@gmail.com>
2020-10-22 17:50:14 -0300

    :sparkles: [#20] Sessão de sorriso

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,7 @@ import { Button, Card, CardBody, CardHeader, Col, Input, Row } from 'reactstrap'
 import { Link } from 'react-router-dom';
 import FittedImage from 'react-fitted-image';
 
-import Preco2 from 'content/images/pages/preco/preco2.png';
+import Home3 from 'content/images/pages/home/home3.jpg';
 import Preco4 from 'content/images/pages/preco/preco4.jpg';
 import Agenda1 from 'content/images/agenda1.png';
 import Home2 from 'content/images/pages/home/home2.png';
@@ -100,42 +100,32 @@ class Home extends React.Component {
         {/*O que está incluso*/}
         <section className="infoContainer">
           <Row className="h-100">
-            <Col md={6} className="h-100">
-              <div id="left-side-box" className="h-100 justify-content-center">
-                <h1 className="font-weight-bold">O que está incluso?</h1>
-                <h2>nosso tratamento já vem com tudo para te fazer sorrir</h2>
-                <ul className="d-flex flex-column">
-                  <li className="mr-auto ml-auto">
-                    <p>Exames da avaliação</p>
-                  </li>
-                  <li className="mr-auto ml-auto">
-                    <p>Simulação do sorriso final</p>
-                  </li>
-                  <li className="mr-auto ml-auto">
-                    <p>Par de placas de contenção</p>
-                  </li>
-                  <li className="mr-auto ml-auto">
-                    <p>Limpeza antes de começar</p>
-                  </li>
-                  <li className="mr-auto ml-auto">
-                    <p>Todas as etapas de alinhadores</p>
-                  </li>
-                  <li className="mr-auto ml-auto">
-                    <p>Consultas de acompanhamento</p>
-                  </li>
-                  <li className="mr-auto ml-auto">
-                    <p>Atendimento 7 dias por semana</p>
-                  </li>
-                </ul>
-                <Button tag={Link} to="/agenda" className="general-button">
-                  Agendar pré-avaliação
-                </Button>
+            <Col md={6} className="h-100 d-flex justify-content-center" style={{}}>
+              <div className="h-100 justify-content-center align-items-center d-flex">
+                <div>
+                  <h1 className="font-weight-bold">aparelho invisível</h1>
+                  <ul className="d-flex flex-column">
+                    <li className="mr-auto ml-auto">
+                      <p>alinhadores transparentes que</p>
+                    </li>
+                    <li className="mr-auto ml-auto">
+                      <p>movimentam seus dentes por fases,</p>
+                    </li>
+                    <li className="mr-auto ml-auto">
+                      <p>com resultados em cerca de 6 meses</p>
+                    </li>
+                  </ul>
+                  <div className="d-flex">
+                    <Button tag={Link} to="/aparelho-invisivel" className="general-button m-auto">
+                      Saiba mais
+                    </Button>
+                  </div>
+                </div>
               </div>
             </Col>
             <Col className="d-flex h-100 align-items-center" md={6}>
-              <div id="right-side-box" className="h-100" />
-              <div className="w-100 position-absolute" id="preco2-photo">
-                <FittedImage fit="cover" src={Preco2} />
+              <div className="w-100 h-100" id="preco2-photo">
+                <FittedImage fit="cover" src={Home3} />
               </div>
             </Col>
           </Row>
