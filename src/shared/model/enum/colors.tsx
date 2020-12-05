0b8245486ac51970fa398f10fc684888efe412d6
Victor Alcântara <victoralcan@gmail.com>
2020-12-04 21:24:17 -0300

    :lipstick: [#20] Ajustes visuais na comparação aparelho alinhador

--- src/shared/model/enum/colors.tsx
+++ src/shared/model/enum/colors.tsx
@@ -4,6 +4,7 @@ export enum ColorPallet {
   pastelOpac = 'rgba(253, 240, 238, 0.4)',
   orange = '#e76034',
   orangeOpac = 'rgba(231, 96, 52, 0.6)',
+  orangeOpac2 = 'rgba(231, 96, 52, 0.2)',
   gray = '#424242',
   grayOpac = 'rgba(66, 66, 66, 0.6)',
   pink = '#c85c74',
