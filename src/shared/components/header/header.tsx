1ffae68561abf38c44fc4b09f6713475cda78097
Victor Alcântara <victoralcan@gmail.com>
2020-12-26 17:48:22 -0300

    :lipstick: [#2] Aprimorando header

--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'styles/header.scss';
 import { Link } from 'react-router-dom';
 import { Brand } from './header-components';
-import { Button, Col, Navbar } from 'reactstrap';
+import { Button, Col, Collapse, Nav, Navbar, NavbarToggler } from 'reactstrap';
+import { ColorPallet } from '../../model/enum/colors';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGripLines } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
   return (
     <Navbar expand="lg" className="header-container fixed-top shadow">
       <Brand />
-      <Link className="header-option" to="/como-funciona" style={{ textDecoration: 'none' }}>
-        <p>COMO FUNCIONA</p>
-      </Link>
-      <Link className="header-option" to="/valores" style={{ textDecoration: 'none' }}>
-        <p>VALORES</p>
-      </Link>
-      <Link className="header-option" to="/agenda" style={{ textDecoration: 'none' }}>
-        <p>AGENDAR PRÉ-AVALIAÇÃO</p>
-      </Link>
-      <Col>
-        <Button tag={Link} to="/chatbot" className="general-button float-right">
-          <h5 className="text-white">SOU UM CANDIDATO?</h5>
-        </Button>
-      </Col>
+      <NavbarToggler
+        onClick={toggle}
+        style={{ backgroundColor: ColorPallet.orange, color: ColorPallet.white, padding: '10px' }}
+        className="text-white"
+      >
+        <FontAwesomeIcon icon={faGripLines} color="white" />
+      </NavbarToggler>
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <Link className="header-option" to="/como-funciona" style={{ textDecoration: 'none' }}>
+            <p>COMO FUNCIONA</p>
+          </Link>
+          <Link className="header-option" to="/valores" style={{ textDecoration: 'none' }}>
+            <p>VALORES</p>
+          </Link>
+          <Link className="header-option" to="/agenda" style={{ textDecoration: 'none' }}>
+            <p>AGENDAR PRÉ-AVALIAÇÃO</p>
+          </Link>
+        </Nav>
+        <Col>
+          <Button tag={Link} to="/chatbot" className="general-button float-right">
+            <h5 className="text-white">SOU UM CANDIDATO?</h5>
+          </Button>
+        </Col>
+      </Collapse>
     </Navbar>
   );
 }
