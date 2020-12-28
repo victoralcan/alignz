5cbee223be95d001d3892e9b307fec3da89ee2aa
Victor Alcântara <victoralcan@gmail.com>
2020-12-27 21:16:49 -0300

    :sparkles: [#7] Passo 3 do formulário

--- src/shared/components/Wizard/Wizard.tsx
+++ src/shared/components/Wizard/Wizard.tsx
@@ -6,6 +6,7 @@ import MultiStep from './MultiStep';
 import { connect } from 'react-redux';
 import Step1 from './steps/Step1';
 import Step2 from './steps/Step2';
+import Step3 from './steps/Step3';
 
 export interface IWizardProps extends StateProps, DispatchProps {}
 
@@ -70,6 +71,7 @@ class Wizard extends React.Component<IWizardProps, IWizardState> {
     const steps = [
       { name: 'Passo 1', component: <Step1 /> },
       { name: 'Passo 2', component: <Step2 /> },
+      { name: 'Passo 3', component: <Step3 /> },
     ];
     return (
       <Row>
