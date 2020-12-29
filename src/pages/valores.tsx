3c05232e4b03f5587a7f899c6c4d72b8d158a44a
Victor Alcântara <victoralcan@gmail.com>
2020-12-29 00:53:40 -0300

    :iphone: [#6] Trabalhando na responsividade da valores

--- src/pages/valores.tsx
+++ src/pages/valores.tsx
@@ -3,6 +3,7 @@ import FittedImage from 'react-fitted-image';
 import Valores1 from 'content/images/pages/valores/valores1.png';
 import Valores2 from 'content/images/pages/valores/valores2.png';
 import Valores3 from '../content/images/pages/valores/valores3.png';
+import Valores4 from '../content/images/pages/valores/valores4.png';
 import ReadySetSmileRight2 from '../content/images/readySetSmileRight2.png';
 import '../styles/pages/valores.css';
 import Duvidas from '../shared/components/Duvidas';
@@ -10,19 +11,24 @@ import ReadySetSmile from '../shared/components/readySetSmile';
 import { Button, Col, Input, Row } from 'reactstrap';
 import { ColorPallet } from '../shared/model/enum/colors';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 class Valores extends React.Component {
   render() {
+    const content = document.getElementById('root');
+    const contentWidth = content ? content.clientWidth : 1024;
+    const mobile = contentWidth <= 992;
     return (
       <>
         {/*Acessível para todos*/}
         <section className="infoContainer">
-          <div className="position-absolute w-100">
-            <FittedImage src={Valores1} style={{ height: '85vh' }} fit="cover" />
+          <div className="bgImg">
+            <FittedImage src={Valores1} fit="cover" />
           </div>
-          <div className="z-index-2 h-100">
-            <div className="h-100 col-6 center-elements pt-5">
-              <div className="m-auto w-50 pt-5">
+          <div className="bgText">
+            <Col md={6} className="h-100 center-elements pt-5">
+              <div className="contentDiv">
                 <h1 className="display-4 font-weight-bold">Acessível para todos</h1>
                 <ul>
                   <li>
@@ -31,36 +37,30 @@ class Valores extends React.Component {
                   </li>
                 </ul>
               </div>
-            </div>
+            </Col>
           </div>
         </section>
         {/*Quer saber se você é um candidato*/}
         <section className="infoContainer">
-          <div className="position-absolute w-100">
-            <FittedImage src={Valores2} style={{ height: '85vh' }} fit="cover" />
+          <div className="bgImg">
+            <FittedImage src={Valores2} fit="cover" />
           </div>
-          <div className="z-index-2 h-100">
+          <div className="bgText">
             <Row className="h-100">
               <Col md={6} className="h-100"></Col>
               <Col md={6} className="h-100 center-elements">
-                <div className="m-auto w-75">
-                  <h1 className="display-4 font-weight-bold text-black">Quer saber se você é um candidato?</h1>
+                <div className="contentDiv">
+                  <h1 className="titleMobile font-weight-bold text-black">Quer saber se você é um candidato?</h1>
                   <Row>
                     <Col md={12}>
                       <ul>
-                        <li>
-                          <Row>
-                            <Col md={6}>
-                              <Input
-                                placeholder="Preencha seu email"
-                                style={{ backgroundColor: ColorPallet.pastel }}
-                                className="mb-3"
-                              />
-                            </Col>
-                            <Col>
-                              <Button className="general-button"></Button>
-                            </Col>
-                          </Row>
+                        <li className="d-flex">
+                          <div className="d-flex w-75 m-auto">
+                            <Input placeholder="Preencha seu email" style={{ backgroundColor: ColorPallet.pastel }} />
+                            <Button className="btn-dark">
+                              <FontAwesomeIcon icon={faChevronRight} color="white" />
+                            </Button>
+                          </div>
                         </li>
                       </ul>
                     </Col>
@@ -88,23 +88,39 @@ class Valores extends React.Component {
         </section>
         {/*Trabalhamos direto da fábrica*/}
         <section className="infoContainer">
-          <div className="position-absolute w-100">
-            <FittedImage src={Valores3} style={{ height: '85vh' }} fit="cover" />
+          <div className="bgImg">
+            <FittedImage src={Valores3} fit="cover" />
           </div>
-          <div className="z-index-2 h-100">
-            <div className="h-100 col-5 d-flex">
-              <div className="m-auto w-100 pl-5">
+          <div className="bgText">
+            <Col md={5} className="h-100 d-flex">
+              <div className="contentDiv">
                 <ul>
                   <li>
-                    <h2>Trabalhamos direto da fábrica com os pacientes, evitando custos desnecessários.</h2>
+                    <h1 className="text-black">
+                      Trabalhamos direto da fábrica com os pacientes, evitando custos desnecessários.
+                    </h1>
                     <div className="clear" />
                   </li>
                 </ul>
               </div>
-            </div>
+            </Col>
           </div>
         </section>
         {/*Como isso funciona*/}
+        <section className="infoContainerFlex">
+          <div className="d-flex">
+            <h1 className="text-black mr-auto ml-auto mb-5">Como isso funciona?</h1>
+          </div>
+          {mobile ? (
+            <section className="infoContainerSmall">
+              <FittedImage src={Valores4} fit="cover" />
+            </section>
+          ) : (
+            <section className="infoContainerSmall">
+              <FittedImage src={Valores4} fit="contain" />
+            </section>
+          )}
+        </section>
         {/*Ready Set Smile*/}
         <ReadySetSmile src={ReadySetSmileRight2} right={false} />
         {/*Dúvidas*/}
