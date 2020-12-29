095bb535ded87ce677e41a7eddaf63c9714223b5
Victor Alcântara <victoralcan@gmail.com>
2020-12-29 20:08:13 -0300

    :truck: Movendo classes do arquivo de clareamento para o home

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ import { loadIcons } from './config/icon-loader';
 import 'styles/global.scss';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import 'styles/pages/clareamento.scss';
 
 loadIcons();
 
