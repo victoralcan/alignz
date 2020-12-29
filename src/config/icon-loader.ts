b888880707b8cc4fcc278cc839066f33f3878b18
Victor Alcântara <victoralcan@gmail.com>
2020-12-29 00:53:40 -0300

    :iphone: [#6] Trabalhando na responsividade da valores

--- src/config/icon-loader.ts
+++ src/config/icon-loader.ts
@@ -5,9 +5,10 @@ import { faPlayCircle } from '@fortawesome/free-solid-svg-icons/faPlayCircle';
 import { faPhone } from '@fortawesome/free-solid-svg-icons/faPhone';
 import { faLongArrowAltDown } from '@fortawesome/free-solid-svg-icons/faLongArrowAltDown';
 import { faGripLines } from '@fortawesome/free-solid-svg-icons/faGripLines';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons/faChevronRight';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 export const loadIcons = () => {
-  library.add(faDollarSign, faPlus, faMinus, faPlayCircle, faPhone, faLongArrowAltDown, faGripLines);
+  library.add(faDollarSign, faPlus, faMinus, faPlayCircle, faPhone, faLongArrowAltDown, faGripLines, faChevronRight);
 };
